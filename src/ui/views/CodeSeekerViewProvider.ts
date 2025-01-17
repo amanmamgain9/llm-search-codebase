0d8c86b97c274dfbfcf18d24fabdc6eebf517632
amanmamgain9 <amanmamgain9@gmail.com>
2025-01-17 11:12:01 +0530

    first checkpoint in development

--- src/ui/views/CodeSeekerViewProvider.ts
+++ src/ui/views/CodeSeekerViewProvider.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import { ICodeSeekerViewProvider, ModelConfig, ModelConfigurationProvider } from '../../types/interfaces';
-
 export class CodeSeekerViewProvider implements ICodeSeekerViewProvider {
     private _view?: vscode.WebviewView;
     private _modelConfig?: ModelConfig;
